b1699eee9e37cb10db249a7274f2521c2351c81a
Naveen <Naveen@Naveens-MacBook-Pro-2.local>
2024-04-14 07:24:01 +0100

    Day 28

---
+++ src/flexiquiz/login/LoginTest.java
@@ -0,0 +1,24 @@
+package flexiquiz.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest 
+{
+	public static void main(String n[])
+	{
+		System.out.println("This is Login Test............................!");
+	}
+	
+	
+	@Test
+	public void openHomePage()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flexiquiz.com/");
+		
+		driver.findElement(By.id("fq-main-menu-auth")).click();
+	}
+}
