55f7f2a3dcbbe40d36139da83a837f0bba743d52
Naveen <Naveen@Naveens-MacBook-Pro-2.local>
2024-04-14 08:10:12 +0100

    Day 28

--- src/flexiquiz/login/LoginTest.java
+++ src/flexiquiz/login/LoginTest.java
@@ -1,9 +1,9 @@
 package flexiquiz.login;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.Test;
 
 public class LoginTest 
 {
